f859f6ff702c9e9ac947670d2274ec37a32e83d5
BKKang <kang.bokyeong92@gmail.com>
2014-11-28 00:55:23 +0900

    Added Rotation, Background, Movement

--- CG_Termproject/CG_Termproject/CG_Termproject.cpp
+++ CG_Termproject/CG_Termproject/CG_Termproject.cpp
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include <glut.h>
 #include <stdio.h>
-
+#include <glext.h>
+static int Time = 0;
 GLuint LoadTexture(const char* filename){
 	GLuint texture;
 	int width, height;
@@ -11,8 +12,10 @@ GLuint LoadTexture(const char* filename){
 	file = fopen(filename, "rb");
 
 	if (file == NULL){printf("no"); return 0;}
-	width = 600;
-	height = 400;
+	
+	width = 160;
+	height = 160;
+
 	data = (unsigned char *)malloc(width * height* 3);
 	fread(data, width * height * 3, 1, file);
 	fclose(file);
@@ -32,7 +35,6 @@ GLuint LoadTexture(const char* filename){
 	glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
 	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST );
 
-
 	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR );
 	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
 	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
@@ -41,25 +43,46 @@ GLuint LoadTexture(const char* filename){
 
 	return texture;
 }
-void display(void){
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	glColor3f(0.0, 0.0, 1.0);
-	glLoadIdentity();
-	gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-	//성 보이는 크기 조절
-	glScalef(0.12, 0.12, 0.12);
-	
+void DrawBg(void){
+	glDisable(GL_CULL_FACE);
+	GLuint texBG[3];
+	GLfloat size = 160.0;
+	texBG[0] = LoadTexture("bg_right.bmp");
+	texBG[1] = LoadTexture("bg_bottom.bmp");
+	texBG[2] = LoadTexture("bg_front.bmp");
+	glEnable(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, texBG[0]);
+	glBegin(GL_QUADS);
+	glNormal3f(0.0, 1.0, 0.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(0.0, 0.0, size);
+	glTexCoord2f(1.0, 1.0); glVertex3f(size, 0.0, size);
+	glTexCoord2f(1.0, 0.0); glVertex3f(size, 0.0, 0.0);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, texBG[1]);
+	glBegin(GL_QUADS);
+	glNormal3f(0.0, 0.0, 1.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(size, 0.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(size, size, 0.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f(0.0, size, 0.0);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, texBG[2]);
+	glBegin(GL_QUADS);
+	glNormal3f(1.0, 0.0, 0.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f(0.0, size, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(0.0, size, size);
+	glTexCoord2f(0.0, 1.0); glVertex3f(0.0, 0.0, size);
+	glEnd();
+}
+
+void DrawCastle(void){
 	glEnable(GL_CULL_FACE);
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	
-	GLfloat light_pos[4] = {1000.0f, 1000.0f, 1000.0f, 1.0f};
-	GLfloat light_dif[4] = {0.2,0.2,0.2,1.0};
-	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_dif);
-	
 	GLUquadricObj *base = gluNewQuadric();
 	
 	gluQuadricDrawStyle(base, GLU_FILL);
@@ -68,12 +91,18 @@ void display(void){
 
 	GLuint texID;
 	texID = LoadTexture("iceTex2.bmp");
-
+	
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, texID);
 	
+	//glTranslatef(40.0, 40.0, 0);
+	glTranslatef(0, 0, -150.0);
+	
+	/*ground floor disk*/
+	glTranslatef(0, 0, 10);
+	gluDisk(base, 0, 25, 20, 1);
 	/*2nd floor*/
-	glTranslatef(0.0, 0.0, 30.0); //height
+	glTranslatef(0.0, 0.0, 20.0); //height
 	gluDisk(base, 0, 20, 6, 1);
 
 	/*2nd floor pillar back,right two*/
@@ -172,15 +201,44 @@ void display(void){
 
 	/*1st floor*/
 	glTranslatef(0.0, 0.0, -30.0); //height of 1st floor + ground floor
-	
+
 	/*ground floor*/
+
 	gluCylinder(base, 30, 25, 10, 20, 1);
 	glTranslatef(0, 0, 10);
-	gluDisk(base, 0, 25, 20, 1);
-	
 	gluCylinder(base, 20, 20, 20, 6, 1);
+}
+void MakeCastle(void){
+	glPushMatrix();
+	glRotatef((GLfloat) Time, 0.0, 0.0, 1.0);
+	float temp = Time/10;
+	glTranslatef(0, 0, temp);
+	DrawCastle();
+	glPopMatrix();
+}
+void display(void){
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	glFlush();
+	glColor3f(0.0, 0.0, 1.0);
+	glLoadIdentity();
+	gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+	
+	//성 보이는 크기 조절
+	glScalef(0.15, 0.15, 0.15);
+	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	
+	GLfloat light_pos[4] = {500.0f, 500.0f, 500.0f, 1.0f};
+	GLfloat light_dif[4] = {0.3,0.3,0.3,1.0};
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_dif);
+
+	//배경그리기
+	DrawBg();
+	MakeCastle();	
+
+	glutSwapBuffers();
 }
 
 void reshape(int w, int h){
@@ -190,15 +248,21 @@ void reshape(int w, int h){
 	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
 	glMatrixMode(GL_MODELVIEW);
 }
-
+void MyTimer(int Value){
+	Time = (Time + 10) % 1800;
+	glutPostRedisplay();
+	glutTimerFunc(40, MyTimer, 1);
+}
 int main(int argc, char **argv){
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(600,600);
-	glutCreateWindow("example");
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glutCreateWindow("Frozen");
+	//glClearColor(1.0, 1.0, 1.0, 1.0);
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
+	glutTimerFunc(30, MyTimer,1);
 	glutMainLoop();
+
 	return 0;
 }
